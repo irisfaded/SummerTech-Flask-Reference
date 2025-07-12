da990ea422a62a0fffb6f09a3475cc75323805f5
Maria <maria.nikolayeva@gmail.com>
2025-07-12 18:51:43 -0400

    created template and static css

--- app/server.py
+++ app/server.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__) # Tells flask that everything it needs is in the current directory. In this case, inside /app
 
-@app.route("/")
+@app.route("/") # @ adds functionalities to the function we define below
 def index():
-    return "Hello World!"
+    return render_template("index.html")
 
 if __name__ =="__main__":
-    app.run(host="0.0.0.0", port=3000, debug=True)
+    app.run(host="0.0.0.0", port=3000, debug=True) # 0.0.0.0 is localhost, aka your computer
